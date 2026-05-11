88c7013cb5eb2762f18287c49cc0aa79ec1d5d50
rustykleather-ux <rustykleather@gmail.com>
2026-05-11 12:43:20 -0500

    Added Features
    Added Features
    1.Firewall Status
    2.Windows Defender Status
    3.Local Admin Uses

--- src/audit.py
+++ src/audit.py
@@ -1,7 +1,18 @@
 import platform
 import csv
+import subprocess
 from datetime import datetime
 
+def run_powershell(command):
+    try:
+        result = subprocess.run(
+            ["powershell","-Command", command],
+            capture_output=True,
+            text=True
+        )
+        return result.stdout.strip()
+    except Exception as e:
+        return f"Error: {e}"
 def get_system_info():
     return {
         "hostname": platform.node(),
@@ -10,7 +21,21 @@ def get_system_info():
         "architecture": platform.machine(),
         "scan_time": datetime.now().isoformat()
     }
+def get_firewall_status():
+    return run_powershell(
+            "Get-NetFirewallProfile | Select-Object Name,Enabled |ConvertTo-Json"
+    )
 
+def get_defender_status():
+    return run_powershell(
+        "Get-MPComputerStatus | Select-Object AMServiceEnabled, AntivirusEnabled, RealTimeProtectionEnabled | ConvertTo-Json"
+    )
+
+def get_local_admins():
+    return run_powershell(
+        "Get-LocalGroupMember | Group Administrators | Select-Object Name, ObjectClass | ConvertTo-Json"
+
+        
 def save_to_csv(data, filename="audit_results.csv"):
     with open(filename, "w", newline="") as file:
         writer = csv.DictWriter(file, fieldnames=data.keys())
@@ -19,7 +44,12 @@ def save_to_csv(data, filename="audit_results.csv"):
 
 def main():
     results = get_system_info()
+    results["firewall_status"] = get_firewall_status()
+    results["defender_status"] =get_defender_status()
+    results["local_admins"] = get_local_admins()
+    
     save_to_csv(results)
+    
     print("Audit complete. Results saved to audit_results.csv")
 
 if __name__ == "__main__":
